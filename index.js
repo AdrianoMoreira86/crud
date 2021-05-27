b2fa1f6531273b9563414ababb7f2bdcb261ce54
AdrianoMoreira86 <adr_0603@yahoo.com.br>
2021-05-27 20:25:53 -0300

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("teste");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+const usuarioAdriano = [{
+    id: 1,
+    nome: 'Adriano',
+    idade: 35
+}];
+
+const usuarios = [usuarioAdriano];
+
+app.get('/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port ${port}');
+});
